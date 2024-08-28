f27e235aa062365cc71f6ffdf6eeb024228da913
Jon Pugh <jon@thinkdrop.net>
2024-08-28 14:12:25 -0400

    Add Hosted Site base class with "database name" field.

---
+++ src/Modules/hosting_site/src/Entity/SiteEntity/HostedSite.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hosting_site\Entity\SiteEntity;
+
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+
+/**
+ * A bundle class for site entities.
+ */
+final class HostedSite extends HostedSiteBase {
+
+  /**
+   * @inheritDoc
+   */
+  static public function propertyFieldDefinitions(EntityTypeInterface $entity_type, $bundle, array $base_field_definitions)
+  {
+    $fields = parent::propertyFieldDefinitions($entity_type, $bundle, $base_field_definitions);
+
+    $fields['database_name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Database Name'))
+      ->setDescription(t('The database name to create for this site.'))
+      ->setRequired(true)
+      ->setReadOnly(true)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'string',
+      ])
+  ;
+
+    return $fields;
+  }
+}
---
+++ src/Modules/hosting_site/src/Entity/SiteEntity/HostedSiteBase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hosting_site\Entity\SiteEntity;
+
+use Drupal\site\Entity\SiteEntity;
+
+/**
+ * A base bundle class for site entities.
+ */
+abstract class HostedSiteBase extends SiteEntity {
+
+}
