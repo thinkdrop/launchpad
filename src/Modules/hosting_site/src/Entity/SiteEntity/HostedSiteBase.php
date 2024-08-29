173385a7caada76e6966433c529b9376f34abfc1
Jon Pugh <jon@thinkdrop.net>
2024-08-29 13:33:29 -0400

    Use "WebAppSiteBundle" so that those fields appear on the bundle.

--- src/Modules/hosting_site/src/Entity/SiteEntity/HostedSiteBase.php
+++ src/Modules/hosting_site/src/Entity/SiteEntity/HostedSiteBase.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Drupal\hosting_site\Entity\SiteEntity;
 
-use Drupal\site\Entity\SiteEntity;
+use Drupal\site\Entity\Bundle\WebAppSiteBundle;
 
 /**
  * A base bundle class for site entities.
  */
-abstract class HostedSiteBase extends SiteEntity {
+abstract class HostedSiteBase extends WebAppSiteBundle {
 
 }
